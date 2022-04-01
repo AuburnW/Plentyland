758650dcaa86e9e83492695ca62403f6e12749b9
Auburn <54520480+Auburn557@users.noreply.github.com>
2022-03-31 21:50:49 -0700

    Implemented translation and scale transforms.

--- ext/a.js
+++ ext/a.js
@@ -280,10 +280,121 @@ ig.module("plugins.plentyland.graphics").requires(
 	}
 
 	class PlentyContextState {
-		computedColor = new Uint8Array(4).fill(255);
-		color = new Uint8Array(4).fill(255);
+		computedColor = Color.createRGBA().fill(255);
+		color = Color.createRGBA().fill(255);
 		globalAlpha = 1;
+		transform = Transform.createMatrix();
+		/** `true` if the current transform matrix is the identity matrix. */
+		isIdentity = true;
 	}
+
+	/**
+	 * Utility functions for manipulating colors.
+	 */
+	class Color {
+		static createRGBA() {
+			return /** @type {Color.RGBA} */(new Uint8Array(4));
+		}
+	}
+
+	/**
+	 * @typedef {Uint8Array & [red: number, green: number, blue: number, alpha: number]} Color.RGBA
+	 */
+
+	/**
+	 * Utility functions for manipulating transform matrices.
+	 */
+	class Transform {
+		/**
+		 * Returns the x component of the given point multiplied by the transform matrix.
+		 * @param {number} x
+		 * @param {number} y
+		 * @param {Transform.Matrix} matrix 
+		 */
+		static getX(x, y, matrix) {
+			return matrix[0] * x + matrix[2] * y + matrix[4];
+		}
+
+		/**
+		 * eturns the y component of the given point multiplied by the transform matrix.
+		 * @param {number} x
+		 * @param {number} y
+		 * @param {Transform.Matrix} matrix 
+		 */
+		static getY(x, y, matrix) {
+			return matrix[1] * x + matrix[3] * y + matrix[5];
+		}
+
+		/**
+		 * Multiplies `a` by `b`, storing the result in `a`.
+		 * @param {Transform.Matrix} a 
+		 * @param {Transform.Matrix} b
+		 */
+		static multiply(a, b) {
+			const r0 = a[0] * b[0] + a[2] * b[1];
+			const r1 = a[1] * b[0] + a[3] * b[1];
+			const r2 = a[0] * b[2] + a[2] * b[3];
+			const r3 = a[1] * b[2] + a[3] * b[3];
+			const r4 = a[0] * b[4] + a[2] * b[5] + a[4];
+			const r5 = a[1] * b[4] + a[3] * b[5] + a[5];
+			a[0] = r0;
+			a[1] = r1;
+			a[2] = r2;
+			a[3] = r3;
+			a[4] = r4;
+			a[5] = r5;
+		}
+
+		/**
+		 * 
+		 * @param {number} x 
+		 * @param {number} y 
+		 * @param {Transform.Matrix} matrix
+		 */
+		static translate(x, y, matrix) {
+			matrix[4] = matrix[0] * x + matrix[2] * y + matrix[4];
+			matrix[5] = matrix[1] * x + matrix[3] * y + matrix[5];
+		}
+
+		/**
+		 * 
+		 * @param {number} x 
+		 * @param {number} y 
+		 * @param {Transform.Matrix} matrix 
+		 */
+		static scale(x, y, matrix) {
+			matrix[0] = matrix[0] * x;
+			matrix[1] = matrix[1] * x;
+			matrix[2] = matrix[2] * y;
+			matrix[3] = matrix[3] * y;
+		}
+
+		static createMatrix() {
+			return /** @type {Transform.Matrix} */(new Float64Array([1, 0, 0, 1, 0, 0]));
+		}
+
+		/**
+		 * 
+		 * @param {Float64Array & [x: number, y: number]} point 
+		 * @param {Transform.Matrix} matrix 
+		 * @param {Float64Array & [x: number, y: number]} result 
+		 */
+		static point(point, matrix, result) {
+			result[0] = matrix[0] * point[0] + matrix[2] * point[1];
+			result[1] = matrix[1] * point[0] + matrix[3] * point[1];
+		}
+	}
+
+	/** 
+	 * @typedef {Float64Array & [
+	 * 		c1r1: number,
+	 * 		c1r2: number,
+	 * 		c2r1: number,
+	 * 		c2r2: number,
+	 * 		c3r1: number,
+	 * 		c3r2: number
+	 * ]} Transform.Matrix
+	 */
 
 	/**
 	 * @typedef {{
@@ -295,11 +406,15 @@ ig.module("plugins.plentyland.graphics").requires(
 	 * An implementation of `CanvasRenderingContext2D` that forwards calls to WebGL in a worker
 	 */
 	class PlentyContext {
-		/** @type {PlentyContextState[]} */
+		/** @type {PlentyContextState[] & [PlentyContextState]} */
 		stateStack = [new PlentyContextState()];
 		stateIndex = 0;
-		/** @type {PlentyContextState} */
-		state = /** @type {any} */(this.stateStack[0]);
+		state = this.stateStack[0];
+
+		/** A place to put inputs to matrix calculations without generating garbage */
+		inputMatrix = Transform.createMatrix();
+		/** A place to put outputs to matrix calculations without generating garbage */
+		outputMatrix = Transform.createMatrix();
 
 		/**
 		 * 
@@ -337,14 +452,32 @@ ig.module("plugins.plentyland.graphics").requires(
 				};
 				allocationMap.set(allocationHash, allocation);
 			}
-			const x1 = (xOffset + dx) * xScaleCanvas;
-			const y1 = (yOffset + dy) * yScaleCanvas;
-			const x2 = x1 + (dw * xScaleCanvas);
-			const y2 = y1 + (dh * yScaleCanvas);
+			const dx2 = dx + dw;
+			const dy2 = dy + dh;
 			const u1 = allocation.x + sx - sourceX;
 			const v1 = allocation.y + sy - sourceY;
 			const u2 = u1 + sw;
 			const v2 = v1 + sh;
+			let x1;
+			let y1;
+			let x2;
+			let y2;
+			const transform = this.state.transform;
+			if (this.state.isIdentity) {
+				x1 = dx;
+				y1 = dy;
+				x2 = dx2;
+				y2 = dy2;
+			} else {
+				x1 = Transform.getX(dx, dy, transform);
+				y1 = Transform.getY(dx, dy, transform);
+				x2 = Transform.getX(dx2, dy2, transform);
+				y2 = Transform.getY(dx2, dy2, transform);
+			}
+			x1 = (xOffset + x1) * xScaleCanvas;
+			y1 = (yOffset + y1) * yScaleCanvas;
+			x2 = (xOffset + x2) * xScaleCanvas;
+			y2 = (yOffset + y2) * yScaleCanvas;
 			let imageDeltaX = deltaX;
 			let imageDeltaY = deltaY;
 			if (interpolation !== null) {
@@ -426,6 +559,9 @@ ig.module("plugins.plentyland.graphics").requires(
 			state.color.set(this.state.color);
 			state.computedColor.set(this.state.computedColor);
 			state.globalAlpha = this.state.globalAlpha;
+			state.transform.set(this.state.transform);
+			state.isIdentity = this.state.isIdentity;
+			this.state = state;
 		}
 
 		restore() {
@@ -444,7 +580,19 @@ ig.module("plugins.plentyland.graphics").requires(
 		 * @param {number} y 
 		 */
 		scale(x, y) {
-			logDraw({ scale: [x, y] });
+			Transform.scale(x, y, this.state.transform);
+			// const input = this.inputMatrix;
+			// const output = this.outputMatrix;
+			// input[0] = x;
+			// input[1] = 0;
+			// input[2] = 0;
+			// input[3] = y;
+			// input[4] = 0;
+			// input[5] = 0;
+			// Transform.multiply(this.state.transform, input, output);
+			// this.outputMatrix = this.state.transform;
+			// this.state.transform = output;
+			this.state.isIdentity = false;
 		}
 
 		beginPath() {
@@ -481,7 +629,8 @@ ig.module("plugins.plentyland.graphics").requires(
 		 * @param {number} y 
 		 */
 		translate(x, y) {
-			logDraw({ translate: [x, y] });
+			Transform.translate(x, y, this.state.transform);
+			this.state.isIdentity = false;
 		}
 
 		/**
